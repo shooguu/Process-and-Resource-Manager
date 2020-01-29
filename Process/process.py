5d70b3d8deee99edff12c18da97e148df7d5109e
Shogo Nakamura <shogonakamura@Shogos-MacBook-Pro.local>
2020-01-28 18:07:27 -0800

    Moved classes to different modules for clarity

--- Process/process.py
+++ Process/process.py
@@ -1,22 +1,31 @@
+from .pcb import PCB
+from .rcb import RCB
+
 class Process:
     def __init__(self, pcb_num: int, rcb_num: int, rl_num: int):
+
         self.PCB = [-1 for item in range(pcb_num)]
         self.RCB = [-1 for item in range(rcb_num)]
         self.RL  = [-1 for item in range(rl_num)]
 
         self.PCB[0] = PCB()
         self.RCB[0] = RCB()
+        self.RL[0] = RL(0)
         
         self.pcb_head = 1
         self.rl_head = 0
 
-    def create(self):
+
+    def create(self, priority_value: int):
+
         self.PCB[self.pcb_head] = PCB()
-        # self.PCB[self.pcb_head].parent = 
+        self.PCB[self.pcb_head].parent = self.pcb_head
         self.PCB[self.pcb_head].children.append(self.pcb_head)
+        self.PCB[self.pcb_head].priority_value = priority_value
         self.RL[self.rl_head] = self.pcb_head
         self.pcb_head += 1
         print(f"process {self.rl_head} created")
+
 
     def destroy(self, process_num: int):
         if self.pcb_head < process_num:
@@ -37,6 +46,9 @@ class Process:
             # Free PCB of process_num
             self.PCB[process_num] = -1
 
+            # Decrease PCB head since the process was destroyed
+            self.pcb_head -= 1
+
 
     def __destroy_rl(self, process_num: int):
         for index in range(len(self.RL)):
@@ -46,42 +58,12 @@ class Process:
 
 
 
-class PCB:
-    def __init__(self):
-        # 1 - Ready State, 0 - Blocked State
-        self.state = 1
-
-        # Parent should be initialized as None
-        self.parent = None
-
-        # Child should be initialized as an empty Linked List (Array)
-        self.children = []
-
-        # Resource should be initialized as an empty Linked List (Array)
-        self.resources = []
-
-    def destroy_children(self):
-        if len(self.children) == 0:
-            return
-        else:
-            self.children.pop()
-            self.destroy_children()
-
-
 class RL:
-    def __init__(self, num_of_processes: int):
-        self.process = [-1 for item in range(num_of_processes)]
-
-class RCB:
-    def __init__(self):
-        # 1 - Free, 0 - Allocated
-        self.state = 1
-
-        # Waitlist should be initialized as an empty Linked List (Array)
-        self.waitlist = []
+    def __init__(self, process_num: int):
+        self.process = [process_num]
 
 
 if __name__ == "__main__":
     p = Process(16, 4, 3)
-    p.create()
+    p.create(0)
     p.destroy(1)
